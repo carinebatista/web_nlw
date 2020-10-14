ae8cfcc574411d5ecadf8077102e4d6777494038
Carine Batista <carinedcb@hotmail.com>
2020-10-14 01:21:11 -0300

    Add orphanages page

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import{FiPlus} from 'react-icons/fi';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
+
 
 function OrphanagesMap(){
     return(
-        <h1>HEllo World</h1> 
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+                    
+                    <h2> Escolha um orfanato no mapa</h2>
+                    <p> Muitas crianças estão esperando a sua visita :) </p>
+                </header>
+
+                <footer>
+                    <strong> Mogi Guaçu</strong>
+                    <span> São Paulo</span>
+                </footer>
+            </aside>
+
+            <div></div> 
+
+            <Link to="" className="create-orphanage"> 
+                <FiPlus size={32} color="#FFF"/> 
+            </Link>
+
+        </div>
     );
 }
 export default OrphanagesMap;
