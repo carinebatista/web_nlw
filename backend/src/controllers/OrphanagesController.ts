12c0d8185e478367badbfd843e7068b54631ee20
Carine Batista <carinedcb@hotmail.com>
2020-10-17 13:02:22 -0300

    Add Controller- MVC and Upload images

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -25,6 +25,7 @@ export default{
 
 
     async create(request : Request, response: Response){
+  
         const{
             name,
             latitude,
@@ -37,6 +38,13 @@ export default{
       
         const orphanagesRepository = getRepository(Orphanage);
         
+        const requestImages = request.files as Express.Multer.File[];
+       
+        const images = requestImages.map(image =>{
+            return {path: image.filename}
+        })
+        
+
         const orphanage = orphanagesRepository.create({
             name,
             latitude,
@@ -45,6 +53,7 @@ export default{
             instructions,
             opening_hours,
             open_on_weekends,
+            images,
         });
     
         await orphanagesRepository.save(orphanage);
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,14 +1,16 @@
 import {Router} from 'express';
+import multer from 'multer';
 
+import uploadConfig from './config/upload';
 import OrphanagesController from './controllers/OrphanagesController';
 
 const routes =Router();
-
+const upload = multer(uploadConfig);
 
 // Index para listar
 routes.get('/orphanages', OrphanagesController.index);
 // Buscar um unico orfanato
 routes.get('/orphanages/:id', OrphanagesController.show);
-routes.post('/orphanages', OrphanagesController.create);
+routes.post('/orphanages', upload.array('images') , OrphanagesController.create);
 
 export default routes;
