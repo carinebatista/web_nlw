724efee2f62dbf458e8d5d40e7f29ca7146dbb7f
Carine Batista <carinedcb@hotmail.com>
2020-10-31 14:22:35 -0300

    Add Button to header

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, StyleSheet, Text} from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler';
 
+import {Feather} from '@expo/vector-icons';
 
 interface HeaderProps {
     title:String;
@@ -9,6 +11,13 @@ interface HeaderProps {
 export default function Header({title}: HeaderProps){
     return(
         <View style={styles.container}>
+            <BorderlessButton onPress={() => {}}> 
+                <Feather 
+                    name="arrow-left"
+                    size={24}
+                    color="#15b6b6"/>
+            </BorderlessButton>
+
             <Text style={styles.title}> {title} </Text>
         </View>
     );
